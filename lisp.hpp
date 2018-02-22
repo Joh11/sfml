2c0239a3e848dfac42cf002bb7be95825d5a7696
Joh11 <johan74470@gmail.com>
2018-02-22 18:44:09 +0100

    hard method backup

--- lisp.hpp
+++ lisp.hpp
@@ -11,62 +11,62 @@
 
 namespace utils
 {
-    template <typename T>
-    std::unique_ptr<T> copy_unique(const std::unique_ptr<T>& source)
-    {
-	return source ? std::make_unique<T>(*source) : nullptr;
-    }
+        template <typename T>
+        std::unique_ptr<T> copy_unique(const std::unique_ptr<T>& source)
+        {
+                return (bool)source ? std::make_unique<T>(*source) : nullptr;
+        }
 }
 
 namespace lisp
 {
-    using Num = double;
-    template <typename T> using List = std::list<T>;
-    
-    struct Atom
-    {
-	enum {NUM, SYM, STR} type;
-	
-	Num num;
-	std::string sym;
-	std::string str;
+        using Num = double;
+        template <typename T> using List = std::list<T>;
 
-	static Atom mkNum(Num const& num) {return {NUM, num, "", ""}; }
-	static Atom mkSym(std::string const& sym) {return {SYM, 0, sym, ""}; }
-	static Atom mkStr(std::string const& str) {return {STR, 0, "", str}; }
-    };
+        struct Atom
+        {
+                enum {NUM, SYM, STR} type;
 
-    std::ostream & operator<<(std::ostream &, Atom const&);
+                Num num;
+                std::string sym;
+                std::string str;
 
-    const Atom EMPTY_ATOM = {Atom::SYM, 0, "", ""};
-    
-    struct Sexpr
-    {
-	bool isAtom;
+                static Atom mkNum(Num const& num) {return {NUM, num, "", ""}; }
+                static Atom mkSym(std::string const& sym) {return {SYM, 0, sym, ""}; }
+                static Atom mkStr(std::string const& str) {return {STR, 0, "", str}; }
+        };
 
-	Atom atom;
-	std::unique_ptr<Sexpr> car, cdr;
+        std::ostream & operator<<(std::ostream &, Atom const&);
 
-	// Copy constructor with deep copy
-	Sexpr(Sexpr const&);
-	Sexpr(bool, Atom const&, std::unique_ptr<Sexpr> const&, std::unique_ptr<Sexpr> const&);
-	Sexpr(bool, Atom const&, Sexpr const& car, Sexpr const& cdr);
+        const Atom EMPTY_ATOM = {Atom::SYM, 0, "", ""};
 
-	static Sexpr mkAtom(Atom const& atom) {return {true, atom, {nullptr}, {nullptr}}; }
-	static Sexpr mkCons(Sexpr const& car, Sexpr const& cdr) {return {false, EMPTY_ATOM, car, cdr}; }
-    };
+        struct Sexpr
+        {
+                bool isAtom;
 
-    std::ostream & operator<<(std::ostream &, Sexpr const&);
+                Atom atom;
+                std::unique_ptr<Sexpr> car, cdr;
 
-    const Sexpr NIL = {true, {Atom::NUM, 0, "NIL", ""}, {nullptr}, {nullptr}};
+                // Copy constructor with deep copy
+                Sexpr(Sexpr const&);
+                Sexpr(bool, Atom const&, std::unique_ptr<Sexpr> const&, std::unique_ptr<Sexpr> const&);
+                Sexpr(bool, Atom const&, Sexpr const& car, Sexpr const& cdr);
 
-    inline bool contains(std::string const& s, char c) {return s.find(c) != s.npos; }
+                static Sexpr mkAtom(Atom const& atom) {return {true, atom, {nullptr}, {nullptr}}; }
+                static Sexpr mkCons(Sexpr const& car, Sexpr const& cdr) {return {false, EMPTY_ATOM, car, cdr}; }
+        };
 
-    List<std::string> tokenize(std::string const& s);
+        std::ostream & operator<<(std::ostream &, Sexpr const&);
 
-    List<Sexpr> parse(std::string const& s);
-    Sexpr parseOne(List<std::string> & tokens);
-    Sexpr mkList(List<Sexpr>);
+        const Sexpr NIL = {true, {Atom::NUM, 0, "NIL", ""}, {nullptr}, {nullptr}};
+
+        inline bool contains(std::string const& s, char c) {return s.find(c) != s.npos; }
+
+        List<std::string> tokenize(std::string const& s);
+
+        List<Sexpr> parse(std::string const& s);
+        Sexpr parseOne(List<std::string> & tokens);
+        Sexpr mkList(List<Sexpr>);
 }
 
 #endif
